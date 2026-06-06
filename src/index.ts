23b663f98d7f744055d7d5cd2a6d162707e7a731
Viktoria Koshman <koshvikt10@gmail.com>
2026-06-06 14:19:45 +0300

    added meals by category

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,30 @@ const URL_MEAL = `https://www.themealdb.com/api/json/v1/1/`;
 
 const container = document.getElementById("meals-container") as HTMLDivElement;
 const popularContainer = document.getElementById("popular") as HTMLDivElement;
+const categoryMeals = document.getElementById(
+  "category-meals",
+) as HTMLDivElement;
 
-getRandomMeal();
-getRandomMeal();
-getRandomMeal();
+getRandomMeal(popularContainer);
+getRandomMeal(popularContainer);
+getRandomMeal(popularContainer);
 
-// async function getMealsByCategory(meal: string) {
-//   if (!meal) return;
-//   const respons = await fetch(`${URL_MEAL}filter.php?c=${meal}`);
-//   const data = await respons.json();
-//   const arr = data.meals;
-//   for (let i = 0; i < arr.length; i++) {
-//     renderMeal(arr[i]);
-//   }
-// }
+async function getMealByID(id: number) {
+  const respons = await fetch(`${URL_MEAL}lookup.php?i=${id}`);
+  const data = await respons.json();
+  return data.meals[0];
+}
+
+async function getMealsByCategory(category: string) {
+  if (!category) return;
+  const respons = await fetch(`${URL_MEAL}filter.php?c=${category}`);
+  const data = await respons.json();
+  const arr = data.meals;
+  for (let i = 0; i < arr.length; i++) {
+    const el = await getMealByID(arr[i].idMeal);
+    renderMeal(el, categoryMeals);
+  }
+}
 
 async function getMeal(meal: string) {
   if (!meal) return;
@@ -28,27 +38,27 @@ async function getMeal(meal: string) {
   const data = await respons.json();
   const arr = data.meals;
   for (let i = 0; i < arr.length; i++) {
-    renderMeal(arr[i]);
+    renderMeal(arr[i], container);
   }
 }
 
-function renderMeal(meal: any) {
+function renderMeal(meal: any, container: HTMLElement) {
   const card = document.createElement("div");
   card.className = "col-md-4";
 
   card.innerHTML = `
   
-    <div class="card h-100 shadow-sm">
+    <div class="card h-100 shadow-sm d-flex flex-column">
       <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}" />
 
-      <div class="card-body">
+      <div class="card-body d-flex flex-column">
         <h5 class="card-title">${meal.strMeal}</h5>
 
-        <p class="card-text">
-          ${meal.strCategory} • ${meal.strArea}
+           <p class="card-text">
+          ${meal.strCategory} • ${meal.strArea !== null ? meal.strArea : ""}
         </p>
 
-        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealModal">
+        <button class="btn btn-primary  mt-auto" data-bs-toggle="modal" data-bs-target="#mealModal">
           Детальніше
         </button>
       </div>
@@ -57,11 +67,14 @@ function renderMeal(meal: any) {
 
   const button = card.querySelector("button")!;
   button.addEventListener("click", () => openModal(meal));
-
-  container.appendChild(card);
+  if (container) {
+    container.appendChild(card);
+  } else {
+    cl("Контейнер не знайдено");
+  }
 }
 
-async function getRandomMeal() {
+async function getRandomMeal(container: HTMLElement) {
   const respons = await fetch(`${URL_MEAL}random.php`);
   const data = await respons.json();
   const element = data.meals[0];
@@ -69,17 +82,17 @@ async function getRandomMeal() {
   card.className = "col-md-4";
 
   card.innerHTML = `
-    <div class="card h-100 shadow-sm">
+    <div class="card h-100 shadow-sm d-flex flex-column">
       <img src="${element.strMealThumb}" class="card-img-top" />
 
-      <div class="card-body">
+      <div class="card-body d-flex flex-column">
         <h5 class="card-title">${element.strMeal}</h5>
 
         <p class="card-text">
-          ${element.strCategory} • ${element.strArea}
+          ${element.strCategory} • ${element.strArea !== null ? element.strArea : ""}
         </p>
 
-        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealModal">
+        <button class="btn btn-primary mt-auto" data-bs-toggle="modal" data-bs-target="#mealModal">
           Детальніше
         </button>
       </div>
@@ -87,7 +100,7 @@ async function getRandomMeal() {
   `;
   const button = card.querySelector("button")!;
   button.addEventListener("click", () => openModal(element));
-  popularContainer.appendChild(card);
+  container.appendChild(card);
 }
 
 function openModal(meal: any) {
@@ -100,7 +113,7 @@ function openModal(meal: any) {
     <img src="${meal.strMealThumb}" class="img-fluid rounded mb-3" />
 
     <p><strong>Категорія:</strong> ${meal.strCategory}</p>
-    <p><strong>Країна:</strong> ${meal.strArea}</p>
+    <p><strong>Країна:</strong> ${meal.strArea !== null ? meal.strArea : ""}</p>
 
     <h4>Інгредієнти</h4>
     <ul>
@@ -143,4 +156,19 @@ form.addEventListener("submit", (e) => {
 
   container.innerHTML = "";
   getMeal(value);
+});
+
+const rand = document.getElementById("rand") as HTMLImageElement;
+rand.addEventListener("click", () => {
+  getRandomMeal(container);
+  container.innerHTML = "";
+});
+
+const caregories = document.querySelectorAll(".category")!;
+
+caregories.forEach((element) => {
+  element.addEventListener("click", () => {
+    categoryMeals.innerHTML = "";
+    getMealsByCategory(element.textContent.trim());
+  });
 });
